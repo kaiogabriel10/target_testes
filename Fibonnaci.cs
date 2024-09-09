e0ac2f2d5f31d2820aed8815d3ca0ad1b6c193c7
Kaio Gabriel <137839907+kaiogabriel10@users.noreply.github.com>
2024-09-09 13:43:27 -0300

    Add files via upload

---
+++ Fibonnaci.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Digite um número para verificar se o mesmo pertence à sequência de Fibonacci:");
+        int numero = int.Parse(Console.ReadLine());
+
+   
+        if (pertenceFibonacci(numero))
+        {
+            Console.WriteLine($"O número {numero} está contido na sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numero} não está contido na sequência de Fibonacci.");
+        }
+    }
+
+    static bool pertenceFibonacci(int numero)
+    { 
+        int a = 0;
+        int b = 1;
+
+        
+        if (numero == a || numero == b)
+        {
+            return true;
+        }
+
+        int proximo_numero = a + b;
+
+        
+        while ( proximo_numero <= numero)
+        {
+            if (proximo_numero == numero)
+            {
+                return true;
+            }
+
+            
+            a = b;
+            b = proximo_numero;
+            proximo_numero = a + b;
+        }
+
+        
+        return false;
+    }
+}
---
+++ VerificacaoExistencia.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testes
+{
+    internal class Test2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma palavra:");
+            string palavra = Console.ReadLine();
+
+            // Indexa a Palavra digitada utilizando o LINQ;
+            var palavra_indexada = palavra.Select(c => c.ToString()).ToList();
+
+            int contagem = 0;
+
+            foreach (var letra in palavra_indexada)
+            {
+                if (letra == "a" || letra == "A")
+                {
+                    contagem += 1;
+                }
+            }
+
+            Console.WriteLine($"O número total de A's na palavra é de {contagem}");
+        }
+    }
+}
